a7da0d7bbb989a58517f6e24a29288632ab547bb
Quentin330 <quentin.menini@gmail.com>
2013-12-08 04:31:00 +0100

    jouer-coup-sans-test un peu plus rapide que jouer-coup

--- iakasparov.lisp
+++ iakasparov.lisp
@@ -5,6 +5,11 @@
 (defparameter alladv 1)
 (defparameter bestadv 0)
 
+;jouer-coup
+(defparameter avectest 1)
+(defparameter sanstest 0)
+
+
 (defun main-affichage (first)
   (let ((tab (init-tab)) (partiefinie NIL))
     (show-tab tab)
@@ -40,19 +45,69 @@
 			(format t "L'IA a gagné ~%")
 			(format t "Egalité ~%")))))))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;la fonction jouer-coup-sans-test est plus rapide et elle permet de supprimer des tests en amont quelques fois, nous allons donc utiliser cette fonction
+(defun test-vitesse-jouer-coup()
+  (let ((tab1 (init-tab))(tab2 (init-tab))(begintime 0) (endtime 0) (time-avec-test)(time-sans-test))
+    (setf begintime (get-internal-real-time))
+    (do ((i 0 (+ i 1)))((= 1000 i))
+      (setf tab1 (init-tab))
+      (jouer-coup-sans-test tab1 1 2 4)
+      (jouer-coup-sans-test tab1 2 2 3)
+      (jouer-coup-sans-test tab1 1 1 2)
+      (jouer-coup-sans-test tab1 2 4 5)
+      (jouer-coup-sans-test tab1 1 5 6)
+      (jouer-coup-sans-test tab1 2 1 4)
+      (jouer-coup-sans-test tab1 1 4 2)
+      (jouer-coup-sans-test tab1 2 5 5)
+      (jouer-coup-sans-test tab1 1 2 5)
+      (jouer-coup-sans-test tab1 2 3 5)
+      (jouer-coup-sans-test tab1 1 6 5))
+    (setf endtime (get-internal-real-time))
+    (setf time-sans-test (- endtime begintime))
+    (format t "Sans tests : ~D millisecondes; " time-sans-test)
+    (setf begintime (get-internal-real-time))
+    (do ((i 0 (+ i 1)))((= 1000 i))
+      (setf tab2 (init-tab))
+      (jouer-coup tab2 1 2 4)
+      (jouer-coup tab2 2 2 3)
+      (jouer-coup tab2 1 1 2)
+      (jouer-coup tab2 2 4 5)
+      (jouer-coup tab2 1 5 6)
+      (jouer-coup tab2 2 1 4)
+      (jouer-coup tab2 1 4 2)
+      (jouer-coup tab2 2 5 5)
+      (jouer-coup tab2 1 2 5)
+      (jouer-coup tab2 2 3 5)
+      (jouer-coup tab2 1 6 5))
+    (setf endtime (get-internal-real-time))
+    (setf time-avec-test (- endtime begintime))
+    (format t "Avec tests : ~D millisecondes : " time-avec-test)
+    (if (> time-avec-test time-sans-test)
+	(format t "Coup plus lent avec test~%")
+	(format t "Coup plus rapide avec test/!\~%"))))
+    
+
+;fin partie de test a supprimer
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    
+
+
 (defun test-temps-de-reponse (nbrtests strat)
   (let ((tab (init-tab)) (partiefinie NIL) (begintime 0) (endtime 0) (nbrovertime 0) (nbrcoups 0))
     (do ((i 0 (+ i 1)))((= i nbrtests))
       (do ((j 0 (+ j 1)))(partiefinie)
-	(setf begintime (get-universal-time))
+	(setf begintime (get-internal-real-time))
 	(jouer-coup-ia tab 1 profondeur :ecrirecoup NIL :strat strat)
-	(setf endtime (get-universal-time))
+	(setf endtime (get-internal-real-time))
 	(setf nbrcoups (+ 1 nbrcoups))
 	(jouer-coup-random tab 2 :ecrirecoup NIL)
-	(if (> (- endtime begintime) 1)
+	(if (> (- endtime begintime) 999)
 	    (progn
-	      (format t "/!\ TEMPS DEPASSE /!\ (~D secondes)~%" (- endtime begintime))
-	      (setf nbrovertime (+ nbrovertime 1))))
+	      (format t "/!\ TEMPS DEPASSE /!\ (~D millisecondes)~%" (- endtime begintime))
+	      (setf nbrovertime (+ nbrovertime 1)))
+	    (format t "Temps non dépassé (~D millisecondes)~%" (- endtime begintime)))
 	(if (fin-partie tab)
 	    (progn
 	      (setf partiefinie T))))
@@ -61,14 +116,15 @@
     (do ((i 0 (+ i 1)))((= i nbrtests))
       (do ((j 0 (+ j 1)))(partiefinie)
 	(jouer-coup-random tab 1 :ecrirecoup NIL)
-	(setf begintime (get-universal-time))
+	(setf begintime (get-internal-real-time))
 	(jouer-coup-ia tab 2 profondeur :ecrirecoup NIL :strat strat)
-	(setf endtime (get-universal-time))
+	(setf endtime (get-internal-real-time))
 	(setf nbrcoups (+ 1 nbrcoups))
 	(if (> (- endtime begintime) 1)
 	    (progn
-	      (format t "/!\ TEMPS DEPASSE /!\ (~D secondes)~%" (- endtime begintime))
-	      (setf nbrovertime (+ nbrovertime 1))))
+	      (format t "/!\ TEMPS DEPASSE /!\ (~D millisecondes)~%" (- endtime begintime))
+	      (setf nbrovertime (+ nbrovertime 1)))
+	    (format t "Temps non dépassé (~D millisecondes)~%" (- endtime begintime)))
 	(if (fin-partie tab)
 	    (progn
 	      (setf partiefinie T))))
@@ -333,141 +389,115 @@
 	    (setf i 7)
 	    (setf (aref tab i j) joueur)))))
 
-;TODO : ne pas utiliser prise-possible (aller dans la direction, si on revoit notre couleur on revient), renvoit NIL si aucun changement, T sinon
+
 (defun jouer-coup-sans-test (tab joueur x y)
-  (let ((tab1 (clone tab))(changed NIL))
-	(if (= 0 (aref tab x y))
-	    (progn
-	      (setf (aref tab x y) joueur)
+  (let ((changed NIL)(eaten NIL))
+    (if (= 0 (aref tab x y))
+	(progn
 ;droite
-	      (if (= (adversaire joueur) (aref tab (+ 1 x) y))
-		  (do ((i (+ 1 x) (+ 1 i)))((> i 6))
-		    (format t "droite")
-		    (if (= (aref tab i y) 0)
-			(setf i 7)
-			(if (= joueur (aref tab i y))
-			    (progn
-			      (setf changed T)
-			      (do ((j (- i 1) (- j 1)))((> j 8))
-				(if (= joueur (aref tab j y))
-				    (setf j 10)
-				    (setf (aref tab j y) joueur)))
-			      (setf i 7))))))
-;gauche
-	      (if (= (adversaire joueur) (aref tab (- x 1) y))
-		  (do ((i (- x 1) (- i 1)))((< i 1))
-		    (format t "gauche")
-		    (if (= (aref tab i y) 0)
-			(setf i 0)
-			(if (= joueur (aref tab i y))
-			    (progn
-			      (setf changed T)
-			      (do ((j (+ i 1) (+ j 1)))((> j 8))
-				(if (= joueur (aref tab j y))
-				    (setf j 10)
-				    (setf (aref tab j y) joueur)))
-			      (setf i 0))))))
-;haut
-	      (if (= (adversaire joueur) (aref tab x (- y 1)))
-		  (do ((i (- y 1) (- i 1)))((< i 1))
-		    (format t "haut")
-		    (if (= (aref tab x i) 0)
-			(setf i 0)
-			(if (= joueur (aref tab x i))
-			    (progn
-			      (setf changed T)
-			      (do ((j (+ i 1) (+ j 1)))((> j 8))
-				(if (= joueur (aref tab x j))
-				    (setf j 10)
-				    (setf (aref tab x j) joueur)))
-			      (setf i 0))))))
-;bas
-	      (if (= (adversaire joueur) (aref tab x (+ y 1)))
-		  (do ((i (+ 1 y) (+ i 1)))((> i 6))
-		    (format t "bas")
-		    (if (= (aref tab x i) 0)
-			(setf i 7)
-			(if (= joueur (aref tab x i))
-			    (progn
-			      (setf changed T)
-			      (do ((j (- i 1) (- j 1)))((> j 8))
-				(if (= joueur (aref tab x j))
-				    (setf j 10)
-				    (setf (aref tab x j) joueur)))
-			      (setf i 7))))))
-;bas droite
-	      (if (= (adversaire joueur) (aref tab (+ x 1) (+ y 1)))
-		  (do ((i (+ 1 x) (+ i 1))(j (+ 1 y) (+ j 1)))
-		      ((or (> i 6) (> j 6)))
-		    (format t "bas droite")
-		    (if (= (aref tab i j) 0)
-			(setf i 7)
-			(if (= joueur (aref tab i j))
-			    (progn
-			      (setf changed T)
-			      (do ((l (- i 1) (- l 1))(h (- j 1) (- h 1)))
-				  ((> l 8))
-				(if (= joueur (aref tab l h))
-				    (setf l 10)
-				    (setf (aref tab l h) joueur)))
-			      (setf i 7))))))
-;bas gauche
-	      (if (= (adversaire joueur) (aref tab (- x 1) (+ y 1)))
-		  (do ((i (- x 1) (- i 1))(j (+ y 1) (+ j 1)))
-		      ((or (< i 1) (> j 6)))
-		    (format t "bas gauche")
-		    (if (= (aref tab i j) 0)
-			(setf i 0)
-			(if (= joueur (aref tab i j))
-			    (progn
-			      (setf changed T)
-			      (do ((l (+ i 1) (+ l 1))(h (- j 1) (- h 1)))
-				  ((> l 8))
-				(if (= joueur (aref tab l h))
-				    (setf l 10)
-				    (setf (aref tab l h) joueur)))
-			      (setf i 0))))))
-;haut gauche
-	      (if (= (adversaire joueur) (aref tab (- x 1) (- y 1)))
-		  (do ((i (- x 1) (- i 1))(j (- y 1) (- j 1)))
-		      ((or (< i 1) (< j 1)))
-		    (format t "haut gauche")
-		    (if (= (aref tab i j) 0)
-			(setf i 0)
-			(if (= joueur (aref tab i j))
-			    (progn
-			      (setf changed T)
-			      (do ((l (+ i 1) (+ l 1))(h (+ j 1) (+ h 1)))
-				  ((> l 8))
-				(if (= joueur (aref tab l h))
-				    (setf l 10)
-				    (setf (aref tab l h) joueur)))
-			      (setf i 0))))))
-;haut droite
-	      (if (= (adversaire joueur) (aref tab (+ x 1) (- y 1)))
-		  (do ((i (+ x 1) (+ i 1))(j (- y 1) (- j 1)))
-		      ((or (> i 6) (< j 1)))
-		    (format t "haut droite")
-		    (if (= (aref tab i j) 0)
-			(setf i 7)
-			(if (= joueur (aref tab i j))
-			    (progn
-			      (setf changed T)
-			      (do ((l (- i 1) (- l 1))(h (+ j 1) (+ h 1)))
-				  ((> l 8))
-				(if (= joueur (aref tab l h))
-				    (setf l 10)
-				    (setf (aref tab l h) joueur)))
-			      (setf i 7))))))
-					;fin
-	      (if changed
-		  T
+	  (if (< x 6)
+	      (if (= (adversaire joueur) (aref tab (+ x 1) y))
 		  (progn
-		    (do ((n 0 (+ n 1))) ((= n 8))
-		      (do ((m 0 (+ m 1))) ((= m 8))
-			(setf (aref tab n m) (aref tab1 n m))))
-		    NIL)))
-	    NIL)))
+		    (do ((i (+ x 1) (+ i 1)))((or (= 8 i) (case-vide tab i y) eaten))
+		      (if (= joueur (aref tab i y))
+			  (progn
+			    (do ((l (- i 1) (- l 1)))((= x l))
+			      (setf (aref tab l y) joueur))
+			    (setf eaten T)
+			    (setf changed T))))
+		    (setf eaten NIL))))
+;gauche
+	  (if (> x 1)
+	      (if (= (adversaire joueur) (aref tab (- x 1) y))
+		  (progn
+		    (do ((i (- x 1) (- i 1)))((or (= 0 i) (case-vide tab i y) eaten))
+		      (if (= joueur (aref tab i y))
+			  (progn
+			    (do ((l (+ i 1) (+ l 1)))((= x l))
+			      (setf (aref tab l y) joueur))
+			    (setf eaten T)
+			    (setf changed T))))
+		    (setf eaten NIL))))
+;haut
+	  (if (> y 1)
+	      (if (= (adversaire joueur) (aref tab x (- y 1)))
+		  (progn
+		    (do ((i (- y 1) (- i 1)))((or (= 0 i) (case-vide tab x i) eaten))
+		      (if (= joueur (aref tab x i))
+			  (progn
+			    (do ((h (+ i 1) (+ h 1)))((= y h))
+			      (setf (aref tab x h) joueur))
+			    (setf eaten T)
+			    (setf changed T))))
+		    (setf eaten NIL))))
+;bas
+	  (if (< y 6)
+	      (if (= (adversaire joueur) (aref tab x (+ y 1)))
+		  (progn
+		    (do ((i (+ y 1) (+ i 1)))((or (= 8 i) (case-vide tab x i) eaten))
+		      (if (= joueur (aref tab x i))
+			  (progn
+			    (do ((h (- i 1) (- h 1)))((= y h))
+			      (setf (aref tab x h) joueur))
+			    (setf eaten T)
+			    (setf changed T))))
+		    (setf eaten NIL))))
+;bas droite
+	  (if (and (< x 6) (< y 6))
+	      (if (= (adversaire joueur) (aref tab (+ x 1) (+ y 1)))
+		  (progn
+		    (do ((i (+ x 1) (+ i 1))(j (+ y 1) (+ j 1))) ((or (= 8 i) (= 8 j) (case-vide tab i j) eaten))
+		      (if (= joueur (aref tab i j))
+			  (progn
+			    (do ((l (- i 1) (- l 1)) (h (- j 1) (- h 1))) ((or (= x l) (= y h)))
+			      (setf (aref tab l h) joueur))
+			    (setf eaten T)
+			    (setf changed T))))
+		    (setf eaten NIL))))
+;bas gauche
+	  (if (and (> x 1) (< y 6))
+	      (if (= (adversaire joueur) (aref tab (- x 1) (+ y 1)))
+		  (progn
+		    (do ((i (- x 1) (- i 1))(j (+ y 1) (+ j 1))) ((or (= 0 i) (= 8 j) (case-vide tab i j) eaten))
+		      (if (= joueur (aref tab i j))
+			  (progn
+			    (do ((l (+ i 1) (+ l 1)) (h (- j 1) (- h 1))) ((or (= x l) (= y h)))
+			      (setf (aref tab l h) joueur))
+			    (setf eaten T)
+			    (setf changed T))))
+		    (setf eaten NIL))))
+;haut droite
+	  (if (and (< x 6) (> y 1))
+	      (if (= (adversaire joueur) (aref tab (+ x 1) (- y 1)))
+		  (progn
+		    (do ((i (+ x 1) (+ i 1))(j (- y 1) (- j 1))) ((or (= 8 i) (= 0 j) (case-vide tab i j) eaten))
+		      (if (= joueur (aref tab i j))
+			  (progn
+			    (do ((l (- i 1) (- l 1)) (h (+ j 1) (+ h 1))) ((or (= x l) (= y h)))
+			      (setf (aref tab l h) joueur))
+			    (setf eaten T)
+			    (setf changed T))))
+		    (setf eaten NIL))))
+;haut gauche
+	  (if (and (> x 1) (> y 1))
+	      (if (= (adversaire joueur) (aref tab (- x 1) (- y 1)))
+		  (progn
+		    (do ((i (- x 1) (- i 1))(j (- y 1) (- j 1))) ((or (= 0 i) (= 0 j) (case-vide tab i j) eaten))
+		      (if (= joueur (aref tab i j))
+			  (progn
+			    (do ((l (+ i 1) (+ l 1)) (h (+ j 1) (+ h 1))) ((or (= x l) (= y h)))
+			      (setf (aref tab l h) joueur))
+			    (setf eaten T)
+			    (setf changed T))))
+		    (setf eaten NIL))))
+
+	  (if changed
+	      (progn
+		(setf (aref tab x y) joueur)
+		T)
+	      NIL))
+	NIL)))
+
 
 (defun fin-partie (tab)
   (and (ne-peut-pas-jouer tab 1) (ne-peut-pas-jouer tab 2)))
@@ -488,49 +518,49 @@
 (defun convert-abscisse ()
   (let ((abscisse (read-char)))
     (if (isspace abscisse)
-	(convert-abscisse))
-    (if (char-equal abscisse #\a)
-	0
-	(if (char-equal abscisse #\b)
-	    1
-	    (if (char-equal abscisse #\c)
-		2
-		(if (char-equal abscisse #\d)
-		    3
-		    (if (char-equal abscisse #\e)
-			4
-			(if (char-equal abscisse #\f)
-			    5
-			    (if (char-equal abscisse #\g)
-				6
-				(if (char-equal abscisse #\h)
-				    7
-				    8))))))))))
+	(convert-abscisse)
+	(if (char-equal abscisse #\a)
+	    0
+	    (if (char-equal abscisse #\b)
+		1
+		(if (char-equal abscisse #\c)
+		    2
+		    (if (char-equal abscisse #\d)
+			3
+			(if (char-equal abscisse #\e)
+			    4
+			    (if (char-equal abscisse #\f)
+				5
+				(if (char-equal abscisse #\g)
+				    6
+				    (if (char-equal abscisse #\h)
+					7
+					8)))))))))))
 
 (defun convert-ordonnee ()
   (let ((ordonnee (read-char)))
     (if (isspace ordonnee)
-	(convert-ordonnee))
-  (if (char-equal ordonnee #\1)
-      0
-      (if (char-equal ordonnee #\2)
-	  1
-	  (if (char-equal ordonnee #\3)
-	      2
-	      (if (char-equal ordonnee #\4)
-		  3
-		  (if (char-equal ordonnee #\5)
-		      4
-		      (if (char-equal ordonnee #\6)
-			  5
-			  (if (char-equal ordonnee #\7)
-			      6
-			      (if (char-equal ordonnee #\8)
-				  7
-				  8))))))))))
+	(convert-ordonnee)
+	(if (char-equal ordonnee #\1)
+	    0
+	    (if (char-equal ordonnee #\2)
+		1
+		(if (char-equal ordonnee #\3)
+		    2
+		    (if (char-equal ordonnee #\4)
+			3
+			(if (char-equal ordonnee #\5)
+			    4
+			    (if (char-equal ordonnee #\6)
+				5
+				(if (char-equal ordonnee #\7)
+				    6
+				    (if (char-equal ordonnee #\8)
+					7
+					8)))))))))))
 
-;version aleatoire
-(defun jouer-coup-random (tab joueur &key (ecrirecoup T))
+;version aleatoire avec test
+(defun jouer-coup-random-old (tab joueur &key (ecrirecoup T))
   (let ((x 8)(y 8))
     (if (not (ne-peut-pas-jouer tab joueur))
 	(progn
@@ -546,22 +576,42 @@
 	)
    ))
 
+;version aleatoire sans test
+(defun jouer-coup-random (tab joueur &key (ecrirecoup T))
+  (let ((x 8)(y 8)(bool NIL))
+    (if (not (ne-peut-pas-jouer tab joueur))
+	(progn
+	  (do ((i 0 (+ i 1)))(bool)
+	    (setf x (random 8))
+	    (setf y (random 8))
+	    (setf bool (jouer-coup-sans-test tab joueur x y)))
+	  (if ecrirecoup
+	      (progn
+		(char-abscisse x)
+		(char-ordonnee y)))
+	  )
+	)
+   ))
+
+
 (defun lire-coup (tab joueur &key (affichage NIL))
   (let ((coupvalide NIL) (x 0) (y 0))
     (if (not (ne-peut-pas-jouer tab joueur))
-	(progn
-	  (do ()(coupvalide)
-	    (setf x (convert-abscisse))
-	    (setf y (convert-ordonnee))
-	    (if (jouer-coup-sans-test tab joueur x y)
-		(progn
-		  (setf coupvalide T))
+	(do ()(coupvalide)
+	  (setf x (convert-abscisse))
+	  (setf y (convert-ordonnee))
+	  (if (and (not (= x 8)) (not (= y 8)))
+	      (if (jouer-coup-sans-test tab joueur x y)
+		(setf coupvalide T)
 		(if affichage
-		    (format t "Coup non valide~%Nouveau coup : "))
-		))
-	  (setf coupvalide NIL))
+		    (progn
+		      (format t "Coup non valide~%")
+		      (format t "Nouveau coup : ")))
+	      )
+	      (if affichage
+		  (format t "Coup impossible car entrée incorrecte, veuillez entrer une lettre entre A et H et un chiffre entre 1 et 8 inclus~%Nouveau coup : "))))
 	(if affichage
-		    (format t "Le joueur ne peut pas jouer~%"))
+	    (format t "Le joueur ne peut pas jouer~%"))
 	)))
 
 (defun compter-pions (tab joueur)
@@ -635,15 +685,16 @@
 	      (progn
 		(char-abscisse x)
 		(char-ordonnee y)))
-	  (jouer-coup tab joueur x y)
+	  (jouer-coup-sans-test tab joueur x y)
 	  )
 	(if affichage
 	    (format t "L'ia ne peut pas jouer~%")))))
 
+
 ;Evalue un coup avec choix des meilleurs coups de l'adversaire
 (defun eval-coup-best-adv (tab joueur profondeur i j)
   (let ((tab2 (clone tab))(nbrcoups 0)(sommecoups 0))
-    (jouer-coup tab2 joueur i j)
+    (jouer-coup-sans-test tab2 joueur i j)
     (if (fin-partie tab2)
 	(if (victoire tab2 joueur)
 	    valeurvictoire
@@ -668,7 +719,7 @@
 ;Evalue un coup avec tous les coups de l'adversaire
 (defun eval-coup-all-adv (tab joueur profondeur i j)
   (let ((tab2 (clone tab))(tab3 (clone tab))(nbrcoups 0)(sommecoups 0))
-    (jouer-coup tab2 joueur i j)
+    (jouer-coup-sans-test tab2 joueur i j)
     (if (fin-partie tab2)
 	(if (victoire tab2 joueur)
 	    valeurvictoire
@@ -678,10 +729,8 @@
 	    (progn
 	      (do ((l 0 (+ l 1)))((= l 8))
 		(do ((h 0 (+ h 1)))((= h 8))
-		  (if (coup-valide tab2 (adversaire joueur) h l)
-		      (progn
-			(setf tab3 (clone tab2))
-			(jouer-coup tab3 (adversaire joueur) h l)
+		  (setf tab3 (clone tab2))
+		  (if (jouer-coup-sans-test tab3 (adversaire joueur) h l)
 			(if (= 1 profondeur)
 			    (progn
 			      (setf sommecoups (+ sommecoups (eval-tab tab3 joueur)))
@@ -692,7 +741,7 @@
 				  (if (coup-valide tab3 joueur x y)
 				      (progn
 					(setf sommecoups (+ sommecoups (eval-coup-best-adv tab3 joueur (- profondeur 2) x y)))
-					(setf nbrcoups (+ nbrcoups 1))))))))))))
+					(setf nbrcoups (+ nbrcoups 1)))))))))))
 	      (if (= nbrcoups 0)
 		  1
 		  (/ sommecoups nbrcoups)))))))
@@ -740,7 +789,7 @@
 (defun compter-victoires (tab joueur i j)
   (assert (coup-valide tab joueur i j))
   (let ((tab2 (clone tab)))
-    (jouer-coup tab2 joueur i j)
+    (jouer-coup-sans-test tab2 joueur i j)
     (if (fin-partie tab2)
 	(progn
 	  (if (> (compter-pions tab2 joueur) (compter-pions tab2 (adversaire joueur)))
@@ -754,7 +803,7 @@
       (do ((j 0 (+ j 1)))((= j 8))
 	(if (coup-valide tab2 (adversaire joueur) i j)
 	    (progn
-	      (jouer-coup tab2 (adversaire joueur) i j)
+	      (jouer-coup-sans-test tab2 (adversaire joueur) i j)
 	      (if (fin-partie tab)
 		    (if (> (compter-pions tab2 joueur) (compter-pions tab2 (adversaire joueur)))
 			(setf somme (+ somme 1)))
